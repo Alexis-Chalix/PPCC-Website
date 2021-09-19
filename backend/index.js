0afe9adbe425b62ab9cd28d7188577999305ba3a
Alexis <chalande.alexis@gmail.com>
2021-09-19 14:39:09 +0200

    API Update

--- backend/index.js
+++ backend/index.js
@@ -9,22 +9,24 @@ const directoryPath = path.join(__dirname, 'image_generator/final_images');
 app.options('*', cors())
 app.use(cors())
 
-app.get("/coins", async (req, res) => {
-    const coins = []
-
+const coins = []
+async function createCoinList() {
     await fs.readdir(directoryPath, function (err, files) {
         if (err) {
             return console.log('Unable to scan directory: ' + err);
         } 
-
         files.forEach(function (file) {
             coins.push(file)
         });
-
-        res.status(200).json(coins)
     });
+}
+
+app.get("/coins", async (req, res) => {
+    res.status(200).json(coins)
 })
 
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
 })
+
+createCoinList()
