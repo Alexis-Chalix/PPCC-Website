8e4ca8a83f345a88004d4c7f430bf8cdab22a999
Alexis <chalande.alexis@gmail.com>
2021-09-19 03:20:57 +0200

    API Call + Search bar creation

--- frontend/script/all-skin.js
+++ frontend/script/all-skin.js
@@ -1,31 +1,44 @@
 function createImagesDiv() {
     const images_container = document.getElementById("images-container");
+    
+    try {
+        let request = new XMLHttpRequest();
+        request.open("GET", "http://localhost:8080/coins");
+        request.send();
+        request.onload = () => {
+            if (request.status === 200) {
+                for (const element of JSON.parse(request.response)) {
+                    let image_div = document.createElement("div");
+                    image_div.classList.add("image");
+                    image_div.id = element.slice(0,-4)
 
-    let image_div = document.createElement("div");
-    image_div.classList.add("image");
-    
-    let image = document.createElement("img");
-    image.src = "/utilities/image_generator/final_images/japan.png"
-    image.alt = "japan"
-    
-    let name = document.createElement("h5");
-    name.innerText = "japan";
-    
-    image_div.appendChild(image);
-    image_div.appendChild(name)
-    
-    images_container.appendChild(image_div);
+                    let image = document.createElement("img");
+                    image.src = `../../backend/image_generator/final_images/${element}`
+                    image.alt = element.slice(0,-4)
+                    
+                    let name = document.createElement("h5");
+                    name.innerText = element.slice(0,-4);
+                    
+                    image_div.appendChild(image);
+                    image_div.appendChild(name)
+                    
+                    images_container.appendChild(image_div);
+                }
+            } else {
+                console.log(`error ${request.status} ${request.statusText}`)
+            }
+        }
+    } catch(e) {
+        console.log(e)
+    }
 }
 
-function test() {
-    console.log("test");
-
-    let reader = new FileReader("/utilities/image_generator/filenames.txt");
-    
-    reader.onload = function(evt) {
-        console.log(evt.target.result);
-    };
-    reader.readAsText("/utilities/image_generator/filenames.txt");
+function searchCoin() {
+    value = document.getElementById("searchBar");
+    console.log(searchBar.value)
 }
 
-document.onload = test()
+window.onload = function() {
+    createImagesDiv()
+    searchCoin()
+}
