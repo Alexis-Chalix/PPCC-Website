a3f768b8601ccf12b7672b9e61e395c92df18a4d
Alexis <chalande.alexis@gmail.com>
2021-09-21 16:36:16 +0200

    API

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,6 @@ const cors = require('cors')
 // Init express app
 const express = require('express')
 const app = express()
-let privateKey  = fs.readFileSync('cert/server.key', 'utf8');
-let certificate = fs.readFileSync('cert/server.crt', 'utf8');
-let credentials = { key: privateKey, cert: certificate }
 
 // Options & Path
 const directoryPath = path.join(__dirname, 'image_generator/final_images');
@@ -34,7 +31,8 @@ app.get("/coins", async (req, res) => {
 })
 
 // Create app
-let httpsServer = https.createServer(credentials, app);
-httpsServer.listen(8080);
+app.listen(8080, () => {
+    console.log("Serveur à l'écoute")
+})
 
 createCoinList()
